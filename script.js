dacf3a9d59b99b79c2fae91dc915cb0467afae37
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-03-17 23:58:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here!
-
+    let itemCount = 1;
+    const container = document.querySelector("#infi-list");
+    function generateElements(n) {
+      for (let i = 0; i < n; i++) {
+        const element = document.createElement("li");
+        element.innerText = `Item ${itemCount++}`;
+        element.className = "item";
+        container.appendChild(element);
+      }
+    }
+    generateElements(10);
+    container.addEventListener('scroll', () => {
+      const maxScrollHeight =
+     container.scrollHeight - container.clientHeight
+      const scrollHeight = container.scrollTop;
+      let reamaingScrollHeight = maxScrollHeight - scrollHeight;
+      // console.log(reamaingScrollHeight)
+      if (reamaingScrollHeight <= 5) {
+        generateElements(10);
+      }
+    })
